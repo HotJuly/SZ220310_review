5727e8a444de6870317659154757125aee0115da
HotJuly <949588380@qq.com>
2022-10-07 15:00:03 +0800

    28.Vue-API(全局配置)

--- 02.代码/2.vue_api/src/App.vue
+++ 02.代码/2.vue_api/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <About></About> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+// 这个是配置对象,他的构造函数是Object,不是组件实例对象
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  a:124,
+  mounted(){
+    // console.log('App',this.$options.a)
+    // console.log(c)
   }
 }
 </script>
